1e334cb5b760191d9b4455d03d43424bfe400908
Kamil Bednarski <kamilarturbednarski@gmail.com>
2022-12-03 13:45:31 +0100

    fix: remove unnecessary dependency

--- src/test/java/com/github/kaarbe/HtmlExtractorTest.java
+++ src/test/java/com/github/kaarbe/HtmlExtractorTest.java
@@ -1,18 +1,15 @@
 package com.github.kaarbe;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@ExtendWith(MockitoExtension.class)
 class HtmlExtractorTest {
 
   @ParameterizedTest
